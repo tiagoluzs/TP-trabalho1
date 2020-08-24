b33013ef473fa58dfb460490728b384d70526baf
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2020-08-24 19:11:19 -0300

    Scripts pra gui e terminal

---
+++ jve.gui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo 'Executando Catalogo class..'
+cd ./src/main/java
+java br.pucrs.tp.Catalogo
+echo 'Fim!'
---
+++ jve.terminal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo 'Executando Main class..'
+cd ./src/main/java
+java br.pucrs.tp.Main
+echo 'Fim!'
